1a05693c51f0906f62ad8bfb99ca2d029d10f7c4
Orest Kostiuk <okostiuk@assurance.com>
2023-05-30 17:55:52 +0200

    fixed routes

--- lib/plugin_authentication/engine.rb
+++ lib/plugin_authentication/engine.rb
@@ -7,12 +7,5 @@ module PluginAuthentication
         include Devise::Controllers::Helpers
       end
     end
-    config.to_prepare do
-      Devise::SessionsController.layout "application"
-      Devise::RegistrationsController.layout "application"
-      Devise::ConfirmationsController.layout "application"
-      Devise::UnlocksController.layout "application"
-      Devise::PasswordsController.layout "application"
-    end
   end
 end
