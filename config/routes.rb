70b715778cf6d57b9a0133173c59a2b1a01d6b5b
Orest Kostiuk <okostiuk@assurance.com>
2023-05-31 08:02:32 +0200

    fixing mistake in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 PluginAuthentication::Engine.routes.draw do
   devise_for :users, class_name: "PluginAuthentication::User", module: :devise, controllers: {
-    registrations: 'plugin_authentication_user/registrations',
-    sessions: 'plugin_authentication_user/sessions',
-    omniauth_callbacks: 'plugin_authentication_user/omniauth_callback'
+    registrations: 'plugin_authentication_users/registrations',
+    sessions: 'plugin_authentication_users/sessions',
+    omniauth_callbacks: 'plugin_authentication_users/omniauth_callback'
   }
 
 end
