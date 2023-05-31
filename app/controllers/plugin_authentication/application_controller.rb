e8f4626c01c83b97614fa84b59189dd4a87b9577
Orest Kostiuk <okostiuk@assurance.com>
2023-05-31 11:16:10 +0200

    fixing mistake in routes

--- app/controllers/plugin_authentication/application_controller.rb
+++ app/controllers/plugin_authentication/application_controller.rb
@@ -1,4 +1,12 @@
 module PluginAuthentication
   class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    skip_before_action :verify_authenticity_token, only: :omniauth_callbacks if :omniauth_callbacks?
+
+    private
+
+    def omniauth_callbacks?
+      is_a?(Devise::OmniauthCallbacksController)
+    end
   end
 end
