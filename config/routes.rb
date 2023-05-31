7ebef690b77258d84e640b56ebf0cf8669157b15
Orest Kostiuk <okostiuk@assurance.com>
2023-05-31 08:11:04 +0200

    fixing mistake in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 PluginAuthentication::Engine.routes.draw do
-  devise_for :users, class_name: "PluginAuthentication::User", module: :devise, controllers: {
+  devise_for :users, class_name: "PluginAuthentication::User", controllers: {
     registrations: 'plugin_authentication_users/registrations',
     sessions: 'plugin_authentication_users/sessions',
     omniauth_callbacks: 'plugin_authentication_users/omniauth_callback'
--- lib/plugin_authentication/engine.rb
+++ lib/plugin_authentication/engine.rb
@@ -7,5 +7,10 @@ module PluginAuthentication
         include Devise::Controllers::Helpers
       end
     end
+    initializer "plugin_authentication.omniauth" do
+      Rails.application.config.middleware.use OmniAuth::Builder do
+        provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+      end
+    end
   end
 end
