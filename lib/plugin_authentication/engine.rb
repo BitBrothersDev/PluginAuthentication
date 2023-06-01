838c1ae081c299f666a9d7192f85fa1d7f9b6f45
Orest Kostiuk <okostiuk@assurance.com>
2023-06-01 08:28:16 +0200

    fixing mistake in routes

--- lib/plugin_authentication/engine.rb
+++ lib/plugin_authentication/engine.rb
@@ -7,10 +7,5 @@ module PluginAuthentication
         include Devise::Controllers::Helpers
       end
     end
-    initializer "plugin_authentication.omniauth" do
-      Rails.application.config.middleware.use OmniAuth::Builder do
-        provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
-      end
-    end
   end
 end
